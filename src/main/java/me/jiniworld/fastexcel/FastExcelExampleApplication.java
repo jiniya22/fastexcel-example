5b7580995fc6b47205e42588735437be5a799f62
jini <jini@jiniworld.me>
2025-05-27 11:22:27 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/me/jiniworld/fastexcel/FastExcelExampleApplication.java
@@ -0,0 +1,12 @@
+package me.jiniworld.fastexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastExcelExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FastExcelExampleApplication.class, args);
+  }
+}
